5adc133c2764814c698bc2b815a1452e46dc82e6
malima98 <matheus.al1998@gmail.com>
2021-07-12 16:13:51 -0300

    Mapeando as entidades para o banco de dados

--- src/DevIO.App/obj/Release/netcoreapp3.1/DevIO.App.AssemblyInfo.cs
+++ src/DevIO.App/obj/Release/netcoreapp3.1/DevIO.App.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-DevIO.App-13982AED-3875-4B2F-8FC5-11CA74DAB790")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevIO.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
---
+++ src/DevIO.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,39 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(p => p.Documento)
+               .IsRequired()
+               .HasColumnType("varchar(14)");
+
+            // 1 : 1 => Fornecedor : Endereco
+
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            // 1 : N => Fornecedor : Produto
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+
+
+        }
+    }
+}
