dc1d0f213a83513a812ce3d9df1d8981b0821e3f
malima98 <matheus.al1998@gmail.com>
2021-07-14 20:15:23 -0300

    [FEATURE] Validando as entidades de negócio

---
+++ src/DevIO.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IFornecedorService
+    {
+        Task Adicionar(Fornecedor fornecedor);
+        Task Atualizar(Fornecedor fornecedor);
+        Task Remover(Guid id);
+        Task AtualizarEndereco(Endereco endereco);
+    }
+}
---
+++ src/DevIO.Business/Interfaces/IProdutoService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DevIO.Business.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IProdutoService
+    {
+        Task Adicionar(Produto produto);
+        Task Atualizar(Produto produto);
+        Task Remover(Guid id);
+    }
+}
